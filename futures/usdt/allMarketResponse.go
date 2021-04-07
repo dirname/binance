2f7a48a674b9de8478a98b8a1cc6362c9c42f008
Mickey (Ah !) <1615258@qq.com>
2021-04-07 13:08:15 +0800

    Update .codecov.yml

--- futures/usdt/allMarketResponse.go
+++ futures/usdt/allMarketResponse.go
@@ -1,7 +1,7 @@
 package futuresusdt
 
-// MarketPriceResponse json parser
-type MarketPriceResponse struct {
+// AllMarketPriceResponse json parser
+type AllMarketPriceResponse struct {
 	EventType       string `json:"e"` // Event type
 	EventTime       int64  `json:"E"` // Event time stamp
 	Symbol          string `json:"s"` // Symbol
@@ -12,8 +12,8 @@ type MarketPriceResponse struct {
 	NextFundingTime int64  `json:"T"` // Next funding time
 }
 
-// MarketPriceCombinedResponse json parser
-type MarketPriceCombinedResponse struct {
-	StreamName string                `json:"stream"` // Stream Name
-	Data       []MarketPriceResponse `json:"data"`   // data
+// AllMarketPriceCombinedResponse json parser
+type AllMarketPriceCombinedResponse struct {
+	StreamName string                   `json:"stream"` // Stream Name
+	Data       []AllMarketPriceResponse `json:"data"`   // data
 }

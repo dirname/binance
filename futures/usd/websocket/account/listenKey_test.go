463cf47db2ccda5e51678df87a38f2aaca30b882
Mickey (Ah !) <1615258@qq.com>
2021-05-17 20:53:43 +0800

    Update listenKey_test.go

--- futures/usd/websocket/account/listenKey_test.go
+++ futures/usd/websocket/account/listenKey_test.go
@@ -27,7 +27,7 @@ func TestNewListenKeyBuilder(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := NewListenKeyBuilder(tt.args.host, tt.args.appKey, tt.args.appSecret); !reflect.DeepEqual(got, tt.want) {
+			if got := NewListenKeyBuilder(tt.args.host, tt.args.appKey, tt.args.appSecret); reflect.DeepEqual(got, tt.want) {
 				t.Errorf("NewListenKeyBuilder() = %v, want %v", got, tt.want)
 			}
 		})
